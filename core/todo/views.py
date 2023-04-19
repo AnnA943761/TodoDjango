c79771d566017d6b1bcc3ad74882ab1ae4f471b7
Tumer124 <egor.bugaev2015@yandex.ru>
2023-04-19 22:00:54 +0700

    create

--- core/todo/views.py
+++ core/todo/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from .models import Todo
 from django.http import HttpResponse
 
-
 def main_page(request):
-	return HttpResponse("Большие выйграши")
+	todos = Todo.objects.all()
+	return render(request, "todo/index.html", {"todos": todos})
